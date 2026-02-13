bd9922236a916be69d6dad1a1d637629bff52c43
Meng Fei Dai <mengfeidai713@gmail.com>
2026-02-13 17:53:43 +0000

    fix(Departamento): Solve a Departamento model error

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Departamento extends Model
 {
-    //
+    protected $table = 'departamentos';
+
+    protected $fillable = [
+        'nombre',
+        'abreviatura',
+    ];
+
+    /**
+     * Relación: Un departamento tiene muchos usuarios
+     */
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
